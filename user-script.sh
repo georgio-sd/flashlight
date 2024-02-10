6e236db0839844f2f173f21d6c12ae752531a490
Samson <babayants.s@gmail.com>
2024-02-09 23:58:54 -0600

    userscript update

--- user-script.sh
+++ user-script.sh
@@ -3,9 +3,10 @@
 # Squid install
 yum install -y squid
 sed -i "s/# should be allowed/# should be allowed\nacl sam_spectrum src 66\.25\.51\.4\/32/" /etc/squid/squid.conf
-sed -i "s/# should be allowed/# should be allowed\nacl rem_work src 83\.237\.9\.26\/32/" /etc/squid/squid.conf
-sed -i "s/# should be allowed/# should be allowed\nacl rem_2 src 213\.108\.220\.120\/32/" /etc/squid/squid.conf
+sed -i "s/# should be allowed/# should be allowed\nacl rem src 83\.237\.9\.26\/32/" /etc/squid/squid.conf
+sed -i "s/# should be allowed/# should be allowed\nacl rem src 213\.108\.220\.120\/32/" /etc/squid/squid.conf
+sed -i "s/# should be allowed/# should be allowed\nacl rem src 91\.188\.0\.0\/16/" /etc/squid/squid.conf
+sed -i "s/# should be allowed/# should be allowed\nacl rem src 37\.19\.73\.162\/32/" /etc/squid/squid.conf
 sed -i "s/# from where browsing should be allowed/# from where browsing should be allowed\nhttp_access allow sam_spectrum/" /etc/squid/squid.conf
-sed -i "s/# from where browsing should be allowed/# from where browsing should be allowed\nhttp_access allow rem_work/" /etc/squid/squid.conf
-sed -i "s/# from where browsing should be allowed/# from where browsing should be allowed\nhttp_access allow rem_2/" /etc/squid/squid.conf
+sed -i "s/# from where browsing should be allowed/# from where browsing should be allowed\nhttp_access allow rem/" /etc/squid/squid.conf
 systemctl enable squid
