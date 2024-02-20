dfa3838362df620fc310ee5844cfe21475b96276
Samson <babayants.s@gmail.com>
2024-02-20 17:08:01 -0600

    squid upd

--- user-script.sh
+++ user-script.sh
@@ -4,7 +4,6 @@
 yum install -y squid
 sed -i "s/# should be allowed/# should be allowed\nacl sam_spectrum src 66\.25\.51\.4\/32/" /etc/squid/squid.conf
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 37\.19\.73\.162\/32/" /etc/squid/squid.conf
-sed -i "s/# should be allowed/# should be allowed\nacl rem src 188\.32\.172\.182\/32/" /etc/squid/squid.conf
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 83\.237\.9\.26\/32/" /etc/squid/squid.conf
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 213\.108\.220\.120\/32/" /etc/squid/squid.conf
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 91\.188\.0\.0\/16/" /etc/squid/squid.conf
