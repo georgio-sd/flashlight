174aa89a4f2dc6ead1ee6adcf68a426c97df16c6
Samson <babayants.s@gmail.com>
2024-03-19 23:15:59 -0500

    Hide proxy in user-script.sh

--- user-script.sh
+++ user-script.sh
@@ -9,4 +9,5 @@ sed -i "s/# should be allowed/# should be allowed\nacl rem src 213\.108\.220\.12
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 91\.188\.0\.0\/16/" /etc/squid/squid.conf
 sed -i "s/# from where browsing should be allowed/# from where browsing should be allowed\nhttp_access allow sam_spectrum/" /etc/squid/squid.conf
 sed -i "s/# from where browsing should be allowed/# from where browsing should be allowed\nhttp_access allow rem/" /etc/squid/squid.conf
+sed -i -e '$a\\n# Hide proxy\nforwarded_for delete\nvia off' /etc/squid/squid.conf
 systemctl enable squid
