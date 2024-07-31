e14994b15db1f20887c49109481b9a3035060ea7
Samson <babayants.s@gmail.com>
2024-07-30 23:13:26 -0500

    fix

--- user-script.sh
+++ user-script.sh
@@ -7,7 +7,7 @@ sed -i "s/# should be allowed/# should be allowed\nacl rem src 37\.19\.73\.162\/
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 83\.237\.9\.26\/32/" /etc/squid/squid.conf
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 213\.108\.220\.120\/32/" /etc/squid/squid.conf
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 91\.188\.0\.0\/16/" /etc/squid/squid.conf
-sed -i "s/# should be allowed/# should be allowed\nacl rem src 46\.39\.251\.205\/16/" /etc/squid/squid.conf
+sed -i "s/# should be allowed/# should be allowed\nacl rem src 46\.39\.251\.205\/32/" /etc/squid/squid.conf
 # Ilya
 sed -i "s/# should be allowed/# should be allowed\nacl rem src 193\.93\.121\.164\/32/" /etc/squid/squid.conf
 sed -i "s/# from where browsing should be allowed/# from where browsing should be allowed\nhttp_access allow sam_spectrum/" /etc/squid/squid.conf
